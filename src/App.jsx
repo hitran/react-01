b99061406288d6da4ad77ea375136be629e1c620
Hi Tran <thuchitran@gmail.com>
2024-01-31 23:04:32 +1100

    - update prettier and eslint rules - add data.js - display content dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import reactImg from './assets/react-core-concepts.png'
+import PropTypes from 'prop-types'
+import reactImg from './assets/config.png'
+import { CORE_CONCEPTS } from './data.js'
 
-function Header () {
+function Header() {
   const reactDescriptions = ['Core', 'Fundamental', 'Crucial']
 
   const getRandomInt = (max) => {
@@ -13,22 +15,50 @@ function Header () {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are going
-        to build!
+        {reactDescriptions[getRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   )
 }
 
-function App () {
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
+CoreConcept.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+}
+
+function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((data, i) => (
+              <CoreConcept
+                key={i}
+                image={data.image}
+                title={data.title}
+                description={data.description}
+              />
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
-  );
+  )
 }
 
 export default App
